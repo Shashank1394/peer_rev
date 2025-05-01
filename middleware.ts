74e736acfb0ec8114f292fe13dd7a31d020bdf8e
Shashank Jadhav <132274771+Shashank1394@users.noreply.github.com>
2025-05-01 23:23:14 +0530

    feat: enhance authentication flow with user verification and session management

---
+++ middleware.ts
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+import { privateRoutes } from "./routes";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth(async (req) => {
+  const isLoggedIn = !!req.auth;
+  const { nextUrl } = req;
+  const url = "http://localhost:3000";
+  const isPrivateRoute = privateRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = nextUrl.pathname.includes("/auth");
+  const isApiRoute = nextUrl.pathname.includes("/api");
+
+  if (isApiRoute) return;
+
+  if (isLoggedIn && isAuthRoute) {
+    return Response.redirect(`${url}/dashboard`);
+  }
+
+  if (isAuthRoute && !isLoggedIn) return;
+
+  if (!isLoggedIn && isPrivateRoute) {
+    return Response.redirect(`${url}/auth/login`);
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
