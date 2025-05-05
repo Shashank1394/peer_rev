79542a4def4b30a19c79fe5957ac4488c0142842
Shashank Jadhav <132274771+Shashank1394@users.noreply.github.com>
2025-05-05 14:25:16 +0530

    feat: enhance dashboard with user session display and improved logout button styling

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,10 @@
-import { signOut } from "@/auth";
+import { auth, signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const Dashboard = async () => {
+  const session = await auth();
+
   return (
     <main className="min-h-screen sticky">
       {/* Navbar */}
@@ -11,13 +13,16 @@ const Dashboard = async () => {
           <h1 className="text-3xl">Dashboard</h1>
         </Link>
         <div className="flex gap-4">
+          <p className="text-2xl">{session?.user?.name}</p>
           <form
             action={async () => {
               "use server";
               await signOut({ redirectTo: "/auth/login" });
             }}
           >
-            <Button type="submit">Logout</Button>
+            <Button type="submit" className="rounded-full">
+              Logout
+            </Button>
           </form>
         </div>
       </nav>
